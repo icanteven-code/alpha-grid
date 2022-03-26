1b24c39a450fb99a0d989388a05c54ccc1a19f1d
Simeon Petrov <petrov.v.simeon@gmail.com>
2022-03-27 00:59:38 +0200

    feat: Added offset classes

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,18 @@ const gulp = require('gulp');
 
 const autoprefixer = require('autoprefixer'),
     sourcemaps = require('gulp-sourcemaps'),
-    postcss = require('gulp-postcss');
+    postcss = require('gulp-postcss'),
+    cssnano = require('cssnano');
 
 function autoprefix() {
     return gulp.src('./dist/agrid.css')
         .pipe(sourcemaps.init())
-        .pipe(postcss([autoprefixer({ grid: true })]))
+        .pipe(postcss([
+            autoprefixer({ grid: true }),
+            cssnano({
+                preset: ['advanced', { zindex: false, discardComments: { removeAll: true } }]
+            })
+        ]))
         .pipe(sourcemaps.write('.'))
         .pipe(gulp.dest('./dist'))
 }
